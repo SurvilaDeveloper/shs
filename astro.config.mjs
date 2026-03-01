7e785e8af9bed193d9b57e59cd1ebf259929ad79
SurvilaDeveloper <surviladeveloper@gmail.com>
2026-03-01 11:56:45 -0300

    sin /shs/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
-
-const repo = "shs";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://surviladeveloper.github.io",
-
-  // SOLO usar base en producción (GitHub Pages)
-  base: import.meta.env.PROD ? `/${repo}/` : "/",
-
+  site: "https://shs.com.ar",
+  base: "/",
   integrations: [react(), sitemap()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
